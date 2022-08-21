036e4f4b332b9743417a61cca3b69076ed34144e
hailang <hailang@webuy.ai>
2022-08-21 17:51:53 +0800

    add CI/CD

--- user-app/src/main/java/com/example/userapp/UserAppApplication.java
+++ user-app/src/main/java/com/example/userapp/UserAppApplication.java
@@ -1,9 +1,7 @@
 package com.example.userapp;
 
-import org.apache.shenyu.jvmti.JVMTI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class UserAppApplication {
@@ -12,11 +10,7 @@ public class UserAppApplication {
      * run with jvm arg: -javaagent:/Users/admin/Desktop/shenyu-client-agent/shenyu-agent/target/shenyu-agent-1.0.0-SNAPSHOT.jar
      */
     public static void main(String[] args) {
-        ConfigurableApplicationContext[] contexts = JVMTI.getInstances(ConfigurableApplicationContext.class);
-        System.out.println("before run->" + contexts.length);
         SpringApplication.run(UserAppApplication.class, args);
-        contexts = JVMTI.getInstances(ConfigurableApplicationContext.class);
-        System.out.println("after run->" + contexts.length);
     }
     
 }
