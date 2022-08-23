8445454cb8882fec7564fae4a8c51c7c3dc70966
hailang <hailang@webuy.ai>
2022-08-23 18:33:58 +0800

    register to spring

--- shenyu-agent-unuse-jvmti/src/main/java/org/apache/shenyu/agent/ShenyuClientAgent2.java
+++ shenyu-agent-unuse-jvmti/src/main/java/org/apache/shenyu/agent/ShenyuClientAgent2.java
@@ -1,8 +1,5 @@
 package org.apache.shenyu.agent;
 
-import org.apache.shenyu.jvmti.JVMTI;
-import org.springframework.context.ConfigurableApplicationContext;
-
 import java.lang.instrument.Instrumentation;
 
 /**
@@ -12,23 +9,5 @@ import java.lang.instrument.Instrumentation;
 public class ShenyuClientAgent2 {
     public static void premain(String arg, Instrumentation inst) {
         System.out.println("shenyu client agent2 loaded");
-        new Thread(() -> {
-            ConfigurableApplicationContext context = JVMTI.getInstance(ConfigurableApplicationContext.class);
-            while (null == context) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException ignored) {
-                }
-                context = JVMTI.getInstance(ConfigurableApplicationContext.class);
-            }
-            while (!context.isActive()) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException ignored) {
-                }
-            }
-            System.out.println("finally available context " + context);
-            //todo 在这里实现组装逻辑
-        }).start();
     }
 }
